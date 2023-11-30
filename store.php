a0a75f01f84625ae9a92346b5af6552a2eaf14aa
aieducational <136823974+aieducational@users.noreply.github.com>
2023-11-30 20:40:30 +0100

    Update store.php

--- store.php
+++ store.php
@@ -1,6 +1,6 @@
 <?php
 $myfile = fopen("location.txt", "w");
-$txt = "lat: " . $GET["lat"]; . "long: " . $GET["long"];
+$txt = "lat: " . $GET["lat"]; . "/nlong: " . $GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
